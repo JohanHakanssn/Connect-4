f69d97a0272468d8703b311caad0940038acc082
Johan Håkansson <johan1hakansson@gmail.com>
2024-08-28 12:09:57 +0200

    to make it work with adding a delay i hade to make the startGameLoop metod to a async function and then add a new metod that returns a promise.

--- typescript/classes/App.ts
+++ typescript/classes/App.ts
@@ -19,6 +19,7 @@ export default class App {
 
   gameMode() {
     console.clear();
+    console.log('Välkommen till 4 i rad!');
     console.log('Välj spelläge: 1 för att spela mot datorn, 2 för att spela mot en annan spelare.');
     const choice = prompt('Ange ditt val: (1 - 2):');
 
@@ -33,6 +34,7 @@ export default class App {
 
   createComputerOpponent() {
     console.clear()
+    console.log('Fyra i rad');
     console.log('Spelare vs Dator');
     this.computer = new Computer('Computer', 'O')
     this.playerX = new Player(prompt('Spelare X:s namn: '), 'X');
@@ -42,25 +44,31 @@ export default class App {
 
   createPlayers() {
     console.clear();
-    console.log('Four-In-A-Row');
+    console.log('Fyra i rad');
     this.playerX = new Player(prompt('Spelare X:s namn: '), 'X')
     this.playerO = new Player(prompt('Spelare O:s namn: '), 'O')
   }
 
+  delayComputerMove(time:number) {
+    return new Promise(resolve => setTimeout(resolve, time))
+  }
 
-  startGameLoop() {
+
+  async startGameLoop() {
     while (!this.board.gameOver) {
       console.clear();
       this.board.render();
-      let player: Player | Computer = this.board.currentPlayerColor === 'X' ? this.playerX : this.playerO;
 
+      let player: Player | Computer = this.board.currentPlayerColor === 'X' ? this.playerX : this.playerO;
       let move;
-      if (player instanceof Computer) {
-        console.log(`${player.name}(${player.color}) gör ett drag...`);
-        move = player.computerMove().toString();
-      } else {
-        move = prompt(`Ange ditt drag ${player.name}(${player.color}) - skriv in kolumn: `)
-      }
+
+       if (player instanceof Computer) {
+         console.log(`${player.name}(${player.color}) väntar på datorns drag.`);
+         await this.delayComputerMove(700)
+         move = player.computerMove().toString();
+       } else {
+         move = prompt(`Ange ditt drag ${player.name}(${player.color}) - skriv in kolumn: `)
+       }
 
       let column = +move.trim() - 1;
 
--- typescript/index.ts
+++ typescript/index.ts
@@ -1,7 +1,4 @@
 // import prompt from './helpers/prompt.js';
 import App from "./classes/App.js";
 
-// let name = prompt('Vad heter du? ');
-// console.log(`Hej ${name}!`);
-
 new App();
